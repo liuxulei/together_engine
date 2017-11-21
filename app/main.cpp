53c789eeb13b624acc7e58216ac3785112af5d0e
liuxulei <liuxulei@gmail.com>
2017-11-21 20:23:50 +0800

    add hash map

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,14 @@
+#include <hash_map>
+using namespace __gnu_cxx;
+
+#include<string>
 #include <iostream>
 using namespace std;
 
 int main(){
-	cout << "Hello, world!" << endl;
+	hash_map<string, string> mymap;
+	mymap.insert(pair<string,string>("hcq", "20"));
+	mymap["sgx"]="24";
+	mymap["sb"]="23";
 	return 1;
 }
